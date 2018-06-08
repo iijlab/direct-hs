c0767a0f06632aed2cd45911b48831e512ee6a31
Yuji Yamamoto <yuji-yamamoto@iij.ad.jp>
2018-06-08 14:37:26 +0900

    Add sample

--- wss-client/README.md
+++ wss-client/README.md
@@ -8,7 +8,17 @@ With [http-client](https://hackage.haskell.org/package/http-client) and [http-cl
 - Support non-TLS connection via an HTTP proxy server (I have to modify the [websockets](https://hackage.haskell.org/package/websockets) package to do that).
 - Add APIs to modify config of both http-client and websockets.
 - Test with a mock server.
+- Wrap existing APIs in [websockets](https://hackage.haskell.org/package/websockets) to hide the lower level.
+
+<!-- Uncomment after uploading on Hackage.
 
 ## Example
 
+An example program is here: [app/Main.hs](app/Main.hs).  
+Build the executable by enabling  build-sample flag:
+
+```bash
+stack install ws-client --flag ws-client:build-sample
+```
+-->
 
--- wss-client/app/Main.hs
+++ wss-client/app/Main.hs
@@ -1,6 +1,27 @@
 module Main where
 
-import Data.String.Strip
+
+import           Control.Monad (forever, unless)
+import           Control.Concurrent  (forkIO, killThread)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import           System.Environment (getArgs)
+import qualified Network.WebSockets.Client as WS
+import qualified Network.WebSockets as WS
+
 
 main :: IO ()
-main = interact strip
+main = do
+  url <- head <$> getArgs
+  WS.withWsClient url $ \conn -> do
+    tid <- forkIO $ forever $ do
+      msg <- WS.receiveData conn
+      T.putStrLn msg
+
+    let loop = do
+          line <- T.getLine
+          unless (T.null line) $
+            WS.sendTextData conn line >> loop
+
+    loop
+    killThread tid
