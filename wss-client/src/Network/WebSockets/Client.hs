3d06267f8aa7fdf37103f4fd1ace1527f5ee7e32
Yuji Yamamoto <yuji-yamamoto@iij.ad.jp>
2018-06-13 16:09:18 +0900

    :art: Format and delete unncessary module

--- wss-client/src/Network/WebSockets/Client.hs
+++ wss-client/src/Network/WebSockets/Client.hs
@@ -6,7 +6,6 @@ module Network.WebSockets.Client
 import qualified Control.Exception as E
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
-import qualified Data.Text as T
 import qualified Network.HTTP.Client as Http
 import qualified Network.HTTP.Client.Internal as Http
 import           Network.HTTP.Client.TLS (tlsManagerSettings)
@@ -21,12 +20,12 @@ withClient url action = do
   withWsClientFromManager man url action
 
 
-withWsClientFromManager :: Http.Manager -> String -> (WS.Connection -> IO a) -> IO a
+withWsClientFromManager
+  :: Http.Manager -> String -> (WS.Connection -> IO a) -> IO a
 withWsClientFromManager man rawUrl action = do
   (isSecure, host, path) <- parseWsUrl rawUrl
 
-  let httpUrl =
-        (if isSecure then "https://" else "http://") ++ host ++ path
+  let httpUrl = (if isSecure then "https://" else "http://") ++ host ++ path
   req <- Http.parseRequest $ "GET " ++ httpUrl
 
   Http.withConnection req man $ \httpConn -> do
@@ -34,39 +33,39 @@ withWsClientFromManager man rawUrl action = do
       ( do
         let r = do
               bs <- Http.connectionRead httpConn
-              return $
-                if BS.null bs
-                  then Nothing
-                  else Just bs
+              return $ if BS.null bs then Nothing else Just bs
 
-            w =
-              maybe
-                (Http.connectionClose httpConn)
-                (Http.connectionWrite httpConn . BSL.toStrict)
+            w = maybe (Http.connectionClose httpConn)
+                      (Http.connectionWrite httpConn . BSL.toStrict)
         WS.makeStream r w
       )
       WS.close
-      (\stream -> do
+      ( \stream -> do
         -- TODO: configure WS.ConnectionOptions
-        WS.runClientWithStream stream host path WS.defaultConnectionOptions [] action
+        WS.runClientWithStream stream
+                               host
+                               path
+                               WS.defaultConnectionOptions
+                               []
+                               action
       )
 
 
 parseWsUrl :: String -> IO (Bool, String, String)
 parseWsUrl raw = do
-  uri <- noteInvalidUrl "Invalid URL given" $ parseURI raw
+  uri  <- noteInvalidUrl "Invalid URL given" $ parseURI raw
   auth <- noteInvalidUrl "No authroity specified" $ uriAuthority uri
   host <- dieWhenEmpty "No host specified" $ uriRegName auth
-  let wss = "wss:"
-      scheme' = uriScheme uri
-      scheme = if null scheme' then wss else scheme'
+  let wss      = "wss:"
+      scheme'  = uriScheme uri
+      scheme   = if null scheme' then wss else scheme'
       isSecure = scheme == wss
   return (isSecure, host, uriPath uri ++ uriQuery uri ++ uriFragment uri)
+ where
+  noteInvalidUrl :: String -> Maybe a -> IO a
+  noteInvalidUrl msg =
+    maybe (E.throwIO $ Http.InvalidUrlException raw msg) return
 
-  where
-    noteInvalidUrl :: String -> Maybe a -> IO a
-    noteInvalidUrl msg = maybe (E.throwIO $ Http.InvalidUrlException raw msg) return
-
-    dieWhenEmpty :: String -> String -> IO String
-    dieWhenEmpty msg "" = E.throwIO $ Http.InvalidUrlException raw msg
-    dieWhenEmpty _ s = return s
+  dieWhenEmpty :: String -> String -> IO String
+  dieWhenEmpty msg "" = E.throwIO $ Http.InvalidUrlException raw msg
+  dieWhenEmpty _   s  = return s
