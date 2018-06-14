994688586abeae8b8121ce4550763447b12a90da
Yuji Yamamoto <yuji-yamamoto@iij.ad.jp>
2018-06-14 16:00:00 +0900

    Use wss-client to support HTTP_PROXY
    And now it's wss-client which parses URLs

--- src/Network/MessagePack/Async/Client/WebSocket.hs
+++ src/Network/MessagePack/Async/Client/WebSocket.hs
@@ -2,69 +2,18 @@
 
 module Network.MessagePack.Async.Client.WebSocket (
     withClient
-  , parseWsUrl
-  , EndpointUrl
   , module Network.MessagePack.Async.Client
   ) where
 
-import qualified Control.Error as Err
-import           Data.Maybe (fromMaybe)
 import qualified Data.Text as T
-import           Network.Socket (withSocketsDo, PortNumber)
-import           Network.URI (parseURI, URI(..), URIAuth(..))
-import qualified Network.WebSockets as Ws
-import           Text.Read (readMaybe)
-import qualified Wuss as Wss
+import qualified Network.WebSockets.Client as Ws
 
 import           Network.MessagePack.Async.Client hiding (withClient)
 import qualified Network.MessagePack.Async.Client as Rpc (withClient)
 
-withClient :: EndpointUrl -> Config -> (Client -> IO a) -> IO a
-withClient (EndpointUrl sec host path port) config action =
-  withSocketsDo $ runWs $ \conn -> do
+withClient :: String -> Config -> (Client -> IO a) -> IO a
+withClient url config action =
+  Ws.withConnection url $ \conn -> do
     Ws.forkPingThread conn 30
     let backend = Backend (Ws.sendBinaryData conn) (Ws.receiveData conn) (Ws.sendClose conn ("Bye!" :: T.Text))
     Rpc.withClient config backend action
-  where
-    runWs =
-      if sec
-        then Wss.runSecureClient host port path
-        else Ws.runClient host (fromIntegral port) path
-
-data EndpointUrl =
-  EndpointUrl
-    { _endpointUrlIsSecure :: !Bool
-    , _endpointUrlHost :: !String
-    , _endpointUrlPath :: !String
-    , _endpointUrlPort :: !PortNumber
-    } deriving (Eq, Show)
-
-parseWsUrl :: String -> Either String EndpointUrl
-parseWsUrl raw = do
-  uri <- noteInvalidUrl "Invalid URL given" $ parseURI raw
-  auth <- noteInvalidUrl "No authroity specified" $ uriAuthority uri
-  host <- dieWhenEmpty "No host specified" $ uriRegName auth
-  let path = uriPath uri
-      wss = "wss:"
-      scheme' = uriScheme uri
-      scheme = if null scheme' then wss else scheme'
-      isSecure = scheme == wss
-      defaultPort = if isSecure then 443 else 80
-  return $
-    EndpointUrl
-      isSecure
-      host
-      (path ++ uriQuery uri)
-      ( fromMaybe defaultPort
-          $ readMaybe
-          $ drop 1 {- drop the first colon -}
-          $ uriPort auth
-      )
-
-  where
-    noteInvalidUrl :: String -> Maybe a -> Either String a
-    noteInvalidUrl msg = Err.note (msg ++ ": " ++ show raw)
-
-    dieWhenEmpty :: String -> String -> Either String String
-    dieWhenEmpty msg "" = Left (msg ++ ": " ++ show raw)
-    dieWhenEmpty _ s = return s
