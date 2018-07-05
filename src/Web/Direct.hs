3ec968eb923239c6919741d2e67e6616f5f606fc
Kazu Yamamoto <kazu@iij.ad.jp>
2018-07-05 14:53:10 +0900

    refactoring.

--- src/Web/Direct.hs
+++ src/Web/Direct.hs
@@ -62,9 +62,10 @@ import qualified Data.Text                                  as T
 import qualified Data.UUID                                  as Uuid
 import qualified System.Random.MWC                          as Random
 
+import qualified Network.MessagePack.Async.Client.WebSocket as Rpc
 import           Web.Direct.Types
 
-import qualified Network.MessagePack.Async.Client.WebSocket as Rpc
+----------------------------------------------------------------
 
 data Config = Config {
     directCreateMessageHandler :: Client -> Message -> Aux -> IO ()
@@ -82,6 +83,71 @@ defaultConfig = Config
     , directLogger               = \_ -> return ()
     , directFormatter            = show
     }
+
+----------------------------------------------------------------
+
+login :: Config
+      -> Rpc.URL
+      -> T.Text -- ^ Login email address for direct.
+      -> T.Text -- ^ Login password for direct.
+      -> IO (Either Exception Client)
+login config url email pass = Rpc.withClient url rpcConfig $ \client -> do
+    idfv <- genIdfv
+
+    let magicConstant = M.ObjectStr ""
+    res <- Rpc.callRpc
+        client
+        "create_access_token"
+        [ M.ObjectStr email
+        , M.ObjectStr pass
+        , M.ObjectStr idfv
+        , M.ObjectStr agentName
+        , magicConstant
+        ]
+    case extractResult res of
+        Right (M.ObjectStr token) ->
+            Right <$> newClient (PersistedInfo token idfv) client
+        Right other -> return $ Left $ UnexpectedReponse other
+        Left  e     -> return $ Left e
+  where
+    rpcConfig = Rpc.defaultConfig
+        { Rpc.requestHandler = \rpcClient mid _method _objs -> do
+             -- sending ACK always
+            sendAck rpcClient mid
+        , Rpc.logger         = directLogger config
+        , Rpc.formatter      = directFormatter config
+        }
+
+extractResult :: Rpc.Result -> Either Exception M.Object
+extractResult = fmapL $ \case
+    err@(M.ObjectMap errorMap) ->
+        let isInvalidEP = lookup (M.ObjectStr "message") errorMap
+                == Just (M.ObjectStr "invalid email or password")
+        in  if isInvalidEP
+                then InvalidEmailOrPassword
+                else UnexpectedReponse err
+    other -> UnexpectedReponse other
+
+----------------------------------------------------------------
+
+genIdfv :: IO T.Text
+genIdfv = do
+    g <- Random.createSystemRandom
+    Uuid.toText
+        <$> (   Uuid.fromWords
+            <$> Random.uniform g
+            <*> Random.uniform g
+            <*> Random.uniform g
+            <*> Random.uniform g
+            )
+
+agentName :: T.Text
+agentName = "bot"
+
+apiVersion :: T.Text
+apiVersion = "1.91"
+
+----------------------------------------------------------------
 
 withClient :: Config -> Rpc.URL -> PersistedInfo -> (Client -> IO a) -> IO a
 withClient config url pInfo action = do
@@ -114,6 +180,20 @@ withClient config url pInfo action = do
         , Rpc.formatter      = directFormatter config
         }
 
+createSession :: Client -> IO ()
+createSession client = do
+    ersp <- Rpc.callRpc (clientRpcClient client)
+                       "create_session"
+                       [ M.ObjectStr $ persistedInfoDirectAccessToken $ clientPersistedInfo client
+                       , M.ObjectStr apiVersion
+                       , M.ObjectStr agentName
+                       ]
+    case ersp of
+        Right rsp -> case fromCreateSession rsp of
+            Just user -> setMe client user
+            Nothing   -> return ()
+        _             -> return ()
+
 subscribeNotification :: Client -> IO ()
 subscribeNotification client = do
     let c = clientRpcClient client
@@ -132,6 +212,19 @@ subscribeNotification client = do
     setTalkRooms client $ fromGetTalks talks
     void $ rethrowingException $ Rpc.callRpc c "get_talk_statuses" []
 
+rethrowingException :: IO (Either M.Object M.Object) -> IO M.Object
+rethrowingException action = do
+    res <- action
+    case extractResult res of
+        Right obj -> return obj
+        Left  e   -> E.throwIO e
+
+----------------------------------------------------------------
+
+sendAck :: Rpc.Client -> R.MessageId -> IO ()
+sendAck rpcClient mid = Rpc.replyRpc rpcClient mid $ Right $ M.ObjectBool True
+
+----------------------------------------------------------------
 
 sendMessage :: Client -> Message -> Aux -> IO MessageId
 sendMessage client req aux = do
@@ -144,93 +237,7 @@ sendMessage client req aux = do
                 _                     -> error "sendMessage" -- fixme
         _ -> error "sendMessage" -- fixme
 
-sendAck :: Rpc.Client -> R.MessageId -> IO ()
-sendAck rpcClient mid = Rpc.replyRpc rpcClient mid $ Right $ M.ObjectBool True
-
-createSession :: Client -> IO ()
-createSession client = do
-    ersp <- Rpc.callRpc (clientRpcClient client)
-                       "create_session"
-                       [ M.ObjectStr $ persistedInfoDirectAccessToken $ clientPersistedInfo client
-                       , M.ObjectStr apiVersion
-                       , M.ObjectStr agentName
-                       ]
-    case ersp of
-        Right rsp -> case fromCreateSession rsp of
-            Just user -> setMe client user
-            Nothing   -> return ()
-        _             -> return ()
-
-login
-    :: Config
-    -> Rpc.URL
-    -> T.Text -- ^ Login email address for direct.
-    -> T.Text -- ^ Login password for direct.
-    -> IO (Either Exception Client)
-login config url email pass = Rpc.withClient url rpcConfig $ \client -> do
-    idfv <- genIdfv
-
-    let magicConstant = M.ObjectStr ""
-    res <- Rpc.callRpc
-        client
-        "create_access_token"
-        [ M.ObjectStr email
-        , M.ObjectStr pass
-        , M.ObjectStr idfv
-        , M.ObjectStr agentName
-        , magicConstant
-        ]
-    case extractResult res of
-        Right (M.ObjectStr token) ->
-            Right <$> newClient (PersistedInfo token idfv) client
-        Right other -> return $ Left $ UnexpectedReponse other
-        Left  e     -> return $ Left e
-  where
-    rpcConfig = Rpc.defaultConfig
-        { Rpc.requestHandler = \rpcClient mid _method _objs -> do
-             -- sending ACK always
-            sendAck rpcClient mid
-        , Rpc.logger         = directLogger config
-        , Rpc.formatter      = directFormatter config
-        }
-
-rethrowingException :: IO (Either M.Object M.Object) -> IO M.Object
-rethrowingException action = do
-    res <- action
-    case extractResult res of
-        Right obj -> return obj
-        Left  e   -> E.throwIO e
-
-
-extractResult :: Rpc.Result -> Either Exception M.Object
-extractResult = fmapL $ \case
-    err@(M.ObjectMap errorMap) ->
-        let isInvalidEP = lookup (M.ObjectStr "message") errorMap
-                == Just (M.ObjectStr "invalid email or password")
-        in  if isInvalidEP
-                then InvalidEmailOrPassword
-                else UnexpectedReponse err
-    other -> UnexpectedReponse other
-
-
-genIdfv :: IO T.Text
-genIdfv = do
-    g <- Random.createSystemRandom
-    Uuid.toText
-        <$> (   Uuid.fromWords
-            <$> Random.uniform g
-            <*> Random.uniform g
-            <*> Random.uniform g
-            <*> Random.uniform g
-            )
-
-
-agentName :: T.Text
-agentName = "bot"
-
-
-apiVersion :: T.Text
-apiVersion = "1.91"
+----------------------------------------------------------------
 
 data Channel = Channel (C.MVar (Message, Aux))
 
