c4bbb80896c981132ae4e611abba9f47f675234f
Yuji Yamamoto <yuji-yamamoto@iij.ad.jp>
2018-10-26 16:33:12 +0900

    :memo: :bug: Fix `direct4b send` command doesn't stop + some docs
    Problem
    ====
    
    `direct4b send` command doesn't stop even after finishing to send a message to direct.
    
    Solution
    ====
    
    Add `directWaitCreateMessageHandler` to `Config` to change `RPC.waitRequestHandler` by the library users.
    
    NOTE
    ===
    
    `direct4b upload` has the same problem.
    So I'm gonna fix after merging https://github.com/iij-ii/direct-hs/pull/42.

--- direct-hs/app/direct4b.hs
+++ direct-hs/app/direct4b.hs
@@ -117,7 +117,7 @@ sendText tid = do
     txt <- TL.stripEnd <$> TL.getContents
     pInfo <- dieWhenLeft . D.deserializeLoginInfo =<< B.readFile jsonFileName
     (EndpointUrl url) <- dieWhenLeft =<< decodeEnv
-    let config = D.defaultConfig { D.directEndpointUrl = url }
+    let config = D.defaultConfig { D.directEndpointUrl = url, D.directWaitCreateMessageHandler = False }
     D.withClient config pInfo $ \client -> forM_ (TL.chunksOf 1024 txt)
         $ \chunk -> D.sendMessage client (D.Txt $ TL.toStrict chunk) tid
 
