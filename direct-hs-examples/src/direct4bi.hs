5953630a73943ff018c89d5dab836a959f1723e1
Restyled.io <commits@restyled.io>
2019-01-04 07:49:32 +0000

    Restyled by brittany

--- direct-hs-examples/src/direct4bi.hs
+++ direct-hs-examples/src/direct4bi.hs
@@ -1,25 +1,29 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
 {-# LANGUAGE StandaloneDeriving #-}
 
-import           Control.Arrow            (second)
-import           Control.Concurrent       (threadDelay)
-import           Control.Error            (note)
-import           Control.Monad            (when)
-import           Control.Monad.IO.Class   (liftIO)
-import qualified Data.ByteString.Lazy     as B
-import           Data.Char                (isSpace)
-import           Data.List                (break)
-import qualified Data.Text                as T
-import qualified System.Console.Haskeline as Hl
-import           System.Environment       (getArgs)
-import qualified System.FilePath          as FP
-import           System.IO                (hFlush, hPutStrLn, stderr, stdout)
-import           Text.Pretty.Simple       (pPrint)
-import           Text.Read                (readMaybe)
+import           Control.Arrow                            ( second )
+import           Control.Concurrent                       ( threadDelay )
+import           Control.Error                            ( note )
+import           Control.Monad                            ( when )
+import           Control.Monad.IO.Class                   ( liftIO )
+import qualified Data.ByteString.Lazy          as B
+import           Data.Char                                ( isSpace )
+import           Data.List                                ( break )
+import qualified Data.Text                     as T
+import qualified System.Console.Haskeline      as Hl
+import           System.Environment                       ( getArgs )
+import qualified System.FilePath               as FP
+import           System.IO                                ( hFlush
+                                                          , hPutStrLn
+                                                          , stderr
+                                                          , stdout
+                                                          )
+import           Text.Pretty.Simple                       ( pPrint )
+import           Text.Read                                ( readMaybe )
 
-import qualified Web.Direct               as D
+import qualified Web.Direct                    as D
 
-import           Common                   hiding (jsonFileName)
+import           Common                            hiding ( jsonFileName )
 
 deriving instance Read D.File
 
@@ -30,7 +34,7 @@ main :: IO ()
 main = do
     jsonFileName <- head <$> getArgs
     let jsonBaseName = FP.takeBaseName jsonFileName
-        histFile = jsonBaseName ++ ".history.txt"
+        histFile     = jsonBaseName ++ ".history.txt"
     pInfo <- dieWhenLeft . D.deserializeLoginInfo =<< B.readFile jsonFileName
     hasT  <- Hl.runInputT Hl.defaultSettings Hl.haveTerminalUI
     let prompt = if hasT then consolePrompt jsonBaseName else ""
@@ -38,7 +42,8 @@ main = do
     D.withClient
         D.defaultConfig
             { D.directCreateMessageHandler     =
-                \_client msg -> printMessage msg >> putStr prompt >> hFlush stdout
+                \_client msg ->
+                    printMessage msg >> putStr prompt >> hFlush stdout
             , D.directWaitCreateMessageHandler = False
             }
         pInfo
@@ -100,19 +105,17 @@ runCommand st client "p" arg = do
     case st of
         Just roomId -> do
             sendMessageLogging client (D.Txt $ T.pack arg) roomId
-        _ ->
-            hPutStrLn stderr noRoomIdConfigured
+        _ -> hPutStrLn stderr noRoomIdConfigured
     return st
 runCommand st client "post" arg = do
     let result = do
-            roomId <- note noRoomIdConfigured st
-            selectA <- note ("Invalid Message object: " ++ show arg) $ readMaybe arg
+            roomId  <- note noRoomIdConfigured st
+            selectA <- note ("Invalid Message object: " ++ show arg)
+                $ readMaybe arg
             return (roomId, selectA)
     case result of
-        Right (roomId, selectA) ->
-            sendMessageLogging client selectA roomId
-        Left emsg     ->
-            hPutStrLn stderr emsg
+        Right (roomId, selectA) -> sendMessageLogging client selectA roomId
+        Left  emsg              -> hPutStrLn stderr emsg
     return st
 runCommand st _client "sleep" arg = do
     case readMaybe arg :: Maybe Double of
@@ -121,7 +124,8 @@ runCommand st _client "sleep" arg = do
     return st
 runCommand st _client "help" _arg = do
     putStrLn "r <talk_room_id>: Switch the current talk room by <talk_room_id>"
-    putStrLn "p <message>: Post <message> as a text message to the current talk room."
+    putStrLn
+        "p <message>: Post <message> as a text message to the current talk room."
     putStrLn "post <message>: Post <message> to the current talk room."
     putStrLn "sleep <seconds>: Sleep for <seconds> seconds."
     putStrLn "quit: Quit this application."
@@ -137,15 +141,13 @@ sendMessageLogging client msg roomId = do
     result <- D.sendMessage client msg roomId
     case result of
         Right mid ->
-            putStrLn
-                $  "Successfully created message#"
-                ++ show mid
-                ++ "."
+            putStrLn $ "Successfully created message#" ++ show mid ++ "."
         Left ex -> putStrLn $ "ERROR creating a message: " ++ show ex
 
 
 noRoomIdConfigured :: String
-noRoomIdConfigured = "No room ID configured! Run r <room_id> to configure current room ID!"
+noRoomIdConfigured =
+    "No room ID configured! Run r <room_id> to configure current room ID!"
 
 
 printMessage :: (D.Message, D.MessageId, D.TalkRoom, D.User) -> IO ()
