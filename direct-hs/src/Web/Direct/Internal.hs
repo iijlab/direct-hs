1bc3b6d0643b54c0b871cf572731846a7863c097
Kazuki Okamoto <kazukiokamoto@iij.ad.jp>
2019-03-25 15:52:48 +0900

    expose

--- direct-hs/src/Web/Direct/Internal.hs
+++ direct-hs/src/Web/Direct/Internal.hs
@@ -1,1 +1,25 @@
-module Web.Direct.Internal where
+module Web.Direct.Internal
+  ( -- * 'Web.Direct.Types'
+    DomainId
+  , TalkId
+  , UserId
+  , MessageId
+  , FileId
+  , FileSize
+  , Domain (..)
+  , TalkType (..)
+  , TalkRoom (..)
+  , User (..)
+  , UploadAuth (..)
+
+    -- * 'Web.Direct.Exception'
+  , callRpcThrow
+  , convertOrThrow
+
+    -- * 'Web.Direct.DirectRPC'
+  , decodeTalkRoom
+  ) where
+
+import Web.Direct.Types
+import Web.Direct.Exception
+import Web.Direct.DirectRPC.Map
