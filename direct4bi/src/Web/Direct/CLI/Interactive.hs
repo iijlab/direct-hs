b1035862a59667c8c1659b2cc0e5e93305d93424
Yuji Yamamoto <yuji-yamamoto@iij.ad.jp>
2019-11-22 17:43:00 +0900

    :new: direct4bi: add commands to answer for question messages

--- direct4bi/src/Web/Direct/CLI/Interactive.hs
+++ direct4bi/src/Web/Direct/CLI/Interactive.hs
@@ -1,58 +1,68 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE CPP                   #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns        #-}
 
 module Web.Direct.CLI.Interactive
     ( defaultMain
     , mainWith
     , noRoomIdConfigured
     , RunCommand
-    , State
+    , State (currentTalk, receivedYesNos, receivedSelects, receivedTasks)
     , defaultRunCommand
     , HelpLine
     , defaultHelpLines
     )
 where
 
-import           Control.Applicative      (optional, (<**>))
-import           Control.Arrow            (second)
-import           Control.Concurrent       (threadDelay)
-import           Control.Error            (note)
-import           Control.Monad            (when)
-import           Control.Monad.IO.Class   (liftIO)
-import qualified Data.ByteString.Lazy     as B
-import           Data.Char                (isSpace)
-import           Data.Foldable            (for_)
-import           Data.List                (sort)
-import qualified Data.Text                as T
-import qualified Data.Text.Lazy           as TL
-import qualified Options.Applicative      as Opt
-import qualified System.Console.Haskeline as Hl
-import           System.Exit              (die)
-import qualified System.FilePath          as FP
-import           System.IO                (hFlush, hPutStrLn, stderr, stdout)
-import           Text.Pretty.Simple       (pPrint, pShow)
-import           Text.Read                (readMaybe)
+import           Control.Applicative        (optional, (<**>))
+import           Control.Arrow              (second)
+import           Control.Concurrent         (threadDelay)
+import           Control.Error              (failWith, failWithM, note)
+import           Control.Monad              (when, (<=<))
+import           Control.Monad.IO.Class     (liftIO)
+import           Control.Monad.Trans.Except (runExceptT, throwE)
+import qualified Data.ByteString.Lazy       as B
+import           Data.Char                  (isSpace)
+import           Data.Foldable              (for_)
+import qualified Data.IntMap.Strict         as IM
+import qualified Data.IORef                 as IOR
+import           Data.List                  (sort)
+import qualified Data.Text                  as T
+import qualified Data.Text.Lazy             as TL
+import qualified Options.Applicative        as Opt
+import qualified System.Console.Haskeline   as Hl
+import           System.Exit                (die)
+import qualified System.FilePath            as FP
+import           System.IO                  (hFlush, hPutStrLn, stderr, stdout)
+import           Text.Pretty.Simple         (pPrint, pShow)
+import           Text.Read                  (readMaybe)
 
 #ifdef mingw32_HOST_OS
-import           GHC.IO.Encoding.CodePage (mkLocaleEncoding)
-import           GHC.IO.Encoding.Failure  (CodingFailureMode (TransliterateCodingFailure))
-import           System.IO                (hSetEncoding)
+import           GHC.IO.Encoding.CodePage   (mkLocaleEncoding)
+import           GHC.IO.Encoding.Failure    (CodingFailureMode (TransliterateCodingFailure))
+import           System.IO                  (hSetEncoding)
 #endif
 
-import qualified Web.Direct               as D
+import qualified Web.Direct                 as D
 
 
 data Args = Args
     { loginInfoFile :: FilePath
     , logMessage    :: Bool
     , dumpMsgpack   :: Bool
-    , initialTalkId :: State
+    , initialTalkId :: Maybe D.TalkId
     } deriving (Eq, Show)
 
 
 type RunCommand = State -> D.Client -> String -> String -> IO State
 
 
-type State = Maybe D.TalkId
+data State = State {
+      currentTalk     :: !(Maybe D.TalkId)
+    , receivedYesNos  :: !(IOR.IORef (IM.IntMap D.YesNoQuestion))
+    , receivedSelects :: !(IOR.IORef (IM.IntMap D.SelectQuestion))
+    , receivedTasks   :: !(IOR.IORef (IM.IntMap D.TaskQuestion))
+    }
 
 
 data TalkRoomAndParticipants = TalkRoomAndParticipants
@@ -72,15 +82,26 @@ mainWith runCommand = do
     pInfo <- dieWhenLeft . D.deserializeLoginInfo =<< B.readFile
         (loginInfoFile args)
     hasT <- Hl.runInputT Hl.defaultSettings Hl.haveTerminalUI
-    let prompt = if hasT then consolePrompt jsonBaseName else ""
+    let getPrompt =
+            if hasT
+              then fmap ((++ "@direct4bi> ") . T.unpack . D.displayName) . D.getMe
+              else const (return "")
+
+    st <- mkInitialState $ initialTalkId args
+
+    let loggingHandler =
+            if logMessage args
+              then \client msg -> do
+                  printMessage msg
+                  putStr =<< getPrompt client
+                  hFlush stdout
+              else D.directCreateMessageHandler D.defaultConfig
 
     D.withClient
         D.defaultConfig
-            { D.directCreateMessageHandler     =
-                if logMessage args
-                    then \_client msg ->
-                        printMessage msg >> putStr prompt >> hFlush stdout
-                    else D.directCreateMessageHandler D.defaultConfig
+            { D.directCreateMessageHandler     = \client msg -> do
+                loggingHandler client msg
+                questionHandler st msg
             , D.directWaitCreateMessageHandler = False
             , D.directLogger                   = if dumpMsgpack args
                                                      then putStrLn
@@ -90,9 +111,10 @@ mainWith runCommand = do
                                                      else D.directFormatter D.defaultConfig
             }
         pInfo
-        ( Hl.runInputT Hl.defaultSettings { Hl.historyFile = Just histFile }
-        . startLoop runCommand (initialTalkId args) prompt
-        )
+        $ \client -> do
+            prompt <- getPrompt client
+            Hl.runInputT Hl.defaultSettings { Hl.historyFile = Just histFile }
+                $ startLoop runCommand st prompt client
   where
     argsInfo = Opt.info
         (options <**> Opt.helper)
@@ -118,11 +140,12 @@ mainWith runCommand = do
                     (Opt.long "dump-msgpack" <> Opt.short 'm' <> Opt.help
                         "Dump all received MsgPack RPC messages"
                     )
-            <*> optional
-                (Opt.option
-                    Opt.auto
-                    (Opt.metavar "TALK_ID" <> Opt.long "talk-room" <> Opt.short 't' <> Opt.help
-                        "Initial talk room ID"
+            <*> (optional
+                    (Opt.option
+                        Opt.auto
+                        (Opt.metavar "TALK_ID" <> Opt.long "talk-room" <> Opt.short 't' <> Opt.help
+                            "Initial talk room ID"
+                        )
                     )
                 )
 
@@ -130,10 +153,8 @@ mainWith runCommand = do
 defaultMain :: IO ()
 defaultMain = mainWith $ defaultRunCommand defaultHelpLines
 
-type Prompt = String
 
-consolePrompt :: String -> String
-consolePrompt = (++ "@direct4bi> ")
+type Prompt = String
 
 
 startLoop :: RunCommand -> State -> Prompt -> D.Client -> Hl.InputT IO ()
@@ -164,10 +185,14 @@ defaultHelpLines :: [HelpLine]
 defaultHelpLines =
     [ "r <talk_room_id>: Switch the current talk room by <talk_room_id>"
     , "p <message>: Post <message> as a text message to the current talk room."
+    , "a <question_id> <yes/no, option_number, done>: Send answer for the message."
     , "leave: Leave the current talk room."
     , "post <message>: Post <message> to the current talk room."
     , "show users: Show the logged-in user and his/her acquaintances."
     , "show rooms: Show the logged-in user's talk rooms and their participants."
+    , "show yesnos: Show received Yes/No question messages with question_id."
+    , "show selects: Show received Select question message with question_ids."
+    , "show tasks: Show received Task messages with question_id."
     , "sleep <seconds>: Sleep for <seconds> seconds."
     , "q: Quit this application."
     , "help: Print this message."
@@ -179,7 +204,7 @@ defaultRunCommand _hs st client "r" arg = case readMaybe arg of
     Just roomId -> do
         rooms <- D.getTalkRooms client
         if any ((== roomId) . D.talkId) rooms
-            then return $ Just roomId
+            then return $ st { currentTalk = Just roomId }
             else do
                 hPutStrLn stderr
                     $  "Room#"
@@ -190,12 +215,73 @@ defaultRunCommand _hs st client "r" arg = case readMaybe arg of
         hPutStrLn stderr $ "Invalid room ID: " ++ show arg
         return st
 defaultRunCommand _hs st client "p" arg = do
-    case st of
+    case currentTalk st of
         Just roomId -> sendMessageLogging client (D.Txt $ T.pack arg) roomId
-        _           -> hPutStrLn stderr noRoomIdConfigured
+        Nothing     -> hPutStrLn stderr noRoomIdConfigured
     return st
+defaultRunCommand _hs st client "a" arg = do
+    either (hPutStrLn stderr) return <=< runExceptT $ do
+        roomId <- failWith noRoomIdConfigured $ currentTalk st
+        (qid, ans) <- parseAnswer
+        case ans of
+            "yes"  -> do
+                D.YesNoQuestion { D.question, D.closingType } <- getYesNoQ qid
+                let msg = D.YesNoA D.YesNoAnswer
+                        { D.question = question
+                        , D.closingType = closingType
+                        , D.response = True
+                        , D.inReplyTo = fromIntegral qid
+                        }
+                liftIO $ sendMessageLogging client msg roomId
+            "no"   -> do
+                D.YesNoQuestion { D.question, D.closingType } <- getYesNoQ qid
+                let msg = D.YesNoA D.YesNoAnswer
+                        { D.question = question
+                        , D.closingType = closingType
+                        , D.response = False
+                        , D.inReplyTo = fromIntegral qid
+                        }
+                liftIO $ sendMessageLogging client msg roomId
+            "done" -> do
+                D.TaskQuestion { D.title, D.closingType } <-
+                    failWithM "No such task found!" (IM.lookup qid <$> IOR.readIORef (receivedTasks st))
+                let msg = D.TaskA D.TaskAnswer
+                        { D.title = title
+                        , D.closingType = closingType
+                        , D.done = True
+                        , D.inReplyTo = fromIntegral qid
+                        }
+                liftIO $ sendMessageLogging client msg roomId
+            _other -> do
+                n <- failWith ("Invalid answer: " ++ show ans) $ readMaybe ans
+                D.SelectQuestion { D.question, D.options, D.closingType } <-
+                    failWithM "No such select question found!" (IM.lookup qid <$> IOR.readIORef (receivedSelects st))
+                let optCount = length options
+                if baseSelectAnswerNumber <= n && n <= optCount
+                  then do
+                    let msg = D.SelectA D.SelectAnswer
+                            { D.question = question
+                            , D.closingType = closingType
+                            , D.options = options
+                            , D.response = n - baseSelectAnswerNumber
+                            , D.inReplyTo = fromIntegral qid
+                            }
+                    liftIO $ sendMessageLogging client msg roomId
+                  else
+                    throwE $ "Invalid answer number (from " ++ show baseSelectAnswerNumber ++ " to " ++ show optCount ++ ")"
+    return st
+  where
+    parseAnswer =
+        case words arg of
+            qidS : ans : _ -> do
+                qid <- failWith "Invalid question ID" (readMaybe qidS)
+                return (qid, ans)
+            _ ->
+                throwE "Invalid answer"
+    getYesNoQ qid =
+        failWithM "No such yes/no question found!" (IM.lookup qid <$> IOR.readIORef (receivedYesNos st))
 defaultRunCommand _hs st client "leave" _arg = do
-    case st of
+    case currentTalk st of
         Just roomId -> do
             result <- D.leaveTalkRoom client roomId
             case result of
@@ -203,11 +289,11 @@ defaultRunCommand _hs st client "leave" _arg = do
                     putStrLn $ "Successfully left room#" ++ show roomId ++ "."
                 Left ex -> hPutStrLn stderr $ "ERROR leaving a room: " ++ show ex
 
-        _ -> hPutStrLn stderr noRoomIdConfigured
+        Nothing -> hPutStrLn stderr noRoomIdConfigured
     return st
 defaultRunCommand _hs st client "post" arg = do
     let result = do
-            roomId  <- note noRoomIdConfigured st
+            roomId  <- note noRoomIdConfigured $ currentTalk st
             selectA <- note ("Invalid Message object: " ++ show arg)
                 $ readMaybe arg
             return (roomId, selectA)
@@ -225,6 +311,25 @@ defaultRunCommand _hs st client "show" "rooms" = do
         pPrint $ TalkRoomAndParticipants talk talkUsers
 
     return st
+
+defaultRunCommand _hs st client "show" "yesnos"  = do
+    ryns <- IOR.readIORef $ receivedYesNos st
+    for_ (IM.toAscList ryns) $ \(qid, D.YesNoQuestion { D.question }) ->
+        putStrLn $ show qid ++ ": " ++ "\"" ++ T.unpack question ++ "\""
+    return st
+
+defaultRunCommand _hs st client "show" "selects" = do
+    ss <- IOR.readIORef $ receivedSelects st
+    for_ (IM.toAscList ss) $ \(qid, D.SelectQuestion { D.question, D.options }) -> do
+        putStrLn $ show qid ++ ": " ++ "\"" ++ T.unpack question ++ "\""
+        for_ (zip [baseSelectAnswerNumber..] options) $ \(n, option) ->
+            putStrLn $ "  " ++ show n ++ ": " ++ "\"" ++ T.unpack option ++ "\""
+    return st
+defaultRunCommand _hs st client "show" "tasks"   = do
+    rts <- IOR.readIORef $ receivedTasks st
+    for_ (IM.toAscList rts) $ \(qid, D.TaskQuestion { D.title }) ->
+        putStrLn $ show qid ++ ": " ++ "\"" ++ T.unpack title ++ "\""
+    return st
 defaultRunCommand _hs st _client "sleep" arg = do
     case readMaybe arg :: Maybe Double of
         Just seconds -> threadDelay $ round $ seconds * 1000 * 1000
@@ -236,6 +341,23 @@ defaultRunCommand hs st _client "help" _arg = do
 defaultRunCommand _hs st _client other arg = do
     hPutStrLn stderr $ "Unknown command " ++ show (other ++ " " ++ arg) ++ "."
     return st
+
+
+questionHandler :: State -> (D.Message, D.MessageId, D.TalkRoom, D.User) -> IO ()
+questionHandler st (msg, mid, _, _) =
+    case msg of
+        D.YesNoQ ynq ->
+            IOR.atomicModifyIORef' (receivedYesNos st) $ \mp -> (IM.insert (fromIntegral mid) ynq mp, ())
+        D.SelectQ sq ->
+            IOR.atomicModifyIORef' (receivedSelects st) $ \mp -> (IM.insert (fromIntegral mid) sq mp, ())
+        D.TaskQ tq ->
+            IOR.atomicModifyIORef' (receivedTasks st) $ \mp -> (IM.insert (fromIntegral mid) tq mp, ())
+        _ ->
+            return ()
+
+
+baseSelectAnswerNumber :: Int
+baseSelectAnswerNumber = 1
 
 
 sendMessageLogging :: D.Client -> D.Message -> D.TalkId -> IO ()
@@ -269,6 +391,12 @@ dieWhenLeft = either exitError return
 
 exitError :: String -> IO a
 exitError emsg = die $ "[ERROR] " ++ emsg ++ "\n"
+
+mkInitialState :: Maybe D.TalkId -> IO State
+mkInitialState mtid = State mtid
+    <$> IOR.newIORef mempty
+    <*> IOR.newIORef mempty
+    <*> IOR.newIORef mempty
 
 
 avoidCodingError :: IO ()
